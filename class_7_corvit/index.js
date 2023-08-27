fa63235f2895303659dc93d3d8ed901d2a15d259
AliShahzad <alishahzadr89@gmail.com>
2023-08-27 19:26:06 +0500

    class_7_corvit

---
+++ class_7_corvit/index.js
@@ -0,0 +1,7 @@
+console.log("ali")
+   
+//  QNO:1 setTimeout function mean how time period u have to have to print name or any assign value
+
+setTimeout(() => {
+ console.log("ali shahzad")    
+}, 2000);
