16ac68e989b0baf467e569f9d740c1e289228482
AliShahzad <alishahzadr89@gmail.com>
2023-08-27 19:20:32 +0500

    Class 6

---
+++ class_6_corvit/index.js
@@ -0,0 +1,92 @@
+//   Q:01 create a variable without assigning any value to it and chexk its value
+
+// let a;
+// console.log(a)
+
+// 
+
+// let value ="name";
+// value =123;
+// value =true;
+// console.log(value)
+
+// create a var with let keyword and then declare with same name using const and see behaviour
+
+// let var ;
+// const var ;
+// console.log(var)
+
+// Qno :04  create a variable with let keyword and than declare with same name 
+// let num1;
+// let num1;
+// console.log(num1)//ERROR
+
+// QNO :05 create two variables one string type and second number then (add num to num);(add string to string);(addstring to number)
+
+// let num2 = "ali";
+// let num3 = 14;
+// console.log(num3+num3)
+// console.log(num2+num2)
+// console.log(num2+num3)
+
+// QNO : 06 
+
+// let value1 = 14;
+// let value2 = "ali";
+// let value3 = true;
+// console.log(value1+value2+value3)
+// console.log(value2+value1+value3)
+// console.log(value2+value3+value1)
+
+// QNO : 07 create name and age and then show them in a sequance 
+
+// let username = "ali"
+// let age = 20;
+// let result = `My Name is ${username} I am ${age} years old`
+// console.log(result)
+
+// QN0 : 08  assign any value and chexk type of string 
+
+// let num3 = "string"; // if we use this bracess  [] so then it will show object
+// console.log(typeof num3)
+
+// QNO :09
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// console.log(arr)
+// arr.pop();
+// console.log(arr)
+// arr.push(20);
+// console.log(arr)
+// arr.splice(8,1);
+// console.log(arr)
+// arr.shift(1,1)
+// console.log(arr)
+// arr.unshift("ali")
+// console.log(arr)
+
+// QNO 11
+
+// let temperature = 19;
+// if(temperature>=27){
+//     console.log("On your AC")
+// }
+// else if(temperature<=20){
+//     console.log("Do not Need to Turn on AC")
+// }
+// else{
+//     console.log("Weather is Cool")
+// }
+
+//  
+
+// let username = "ali";
+// let age =20;
+// let result =`MY Name is ${username} I am ${age} year old`;
+// console.log(result)
+
+// let username1 ="Bilal";
+// let age1="";
+// let result1 =`My name is ${username1} I am ${age1} year old`;
+// console.log(result1)
+
